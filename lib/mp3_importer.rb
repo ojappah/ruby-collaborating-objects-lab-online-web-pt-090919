f87145331d2eaadd382f4e5cf5577e6f43d758ad
generous-vector-9581 <ojappah@gmail.com>
2019-10-16 18:24:19 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,15 +1,15 @@
 class MP3Importer
-  attr_accessor :path
+  attr_reader :path
 
   def initialize(path)
     @path = path
   end
 
   def files
-    Dir[@path+"/*.mp3"].map { |file| file.split("/").last }
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |f| f.gsub("#{path}/", "") }
   end
 
   def import
-    files.each { |file| Song.new_by_filename(file) }
+    files.each{|f| Song.new_by_filename(f)}
   end
 end
