1459731de0bd298497f049a4ae91fd9fc33ccd8e
generous-vector-9581 <ojappah@gmail.com>
2019-10-16 18:15:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,62 +1,37 @@
 class Artist
+  attr_accessor :name
 
-  attr_accessor :name, :songs
+  @@all = []
 
-  @@all = [] #array of artists instances
-
-  def initialize(name)#artist_name
+  def initialize(name)
     @name = name
-    @songs = [] #array of songs for each instance
+    @songs= []
   end
 
-  #this exposes the @@all Class Variable
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    song
+  end
+
+  def songs
+    @songs
+  end
+
+  def save
+    @@all << self
+    self
+  end
+
   def self.all
     @@all
   end
 
-  def add_song(song)
-    #called by the song instance, adds in that song instance
-    #called inside the song method #artist_name=
-    #after the song has been created, assigned a name
-    #the song class checks to see if there is an artist associated with the song, and if there isnt' one, it asks the Artist class to create an artist instance
-    #it then passes that artist instance back to the song to store as an attribute
-    #the song class then calls this method #add_song
-    #this method adds the song to the artist instances song array
-    #every artist instance has this array
-    @songs << song
-  end
-
   def self.find_or_create_by_name(name)
-    if self.find(name)
-      self.find(name)
-    else
-      self.create(name)
-    end
+    self.all.detect {|artist| artist.name == name} || Artist.new(name).save
   end
 
-
-  def self.find(name)
-    @@all.find do |artist|
-      artist.name == name
-    end
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    @@all << artist
-    artist
-  end
-
-
-
-  def save
-    @@all << self
-  end
-
-  #this method prints out an array of song names
-  #by accessing the name attribute of each song
-  def print_songs()
-    puts @songs.collect {|x| x.name}
-  end
-
+  def print_songs
+    self.songs.each {|song| puts song.name} 
+   end
 end
