884dba60ff8ccb361402a518fa52ff0cbb21127e
generous-vector-9581 <ojappah@gmail.com>
2019-10-16 18:23:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,46 +1,37 @@
-
 class Artist
-  attr_accessor :name, :songs
-
   @@all = []
+  attr_accessor :name, :songs
 
   def initialize(name)
     @name = name
-    @songs = []
     save
   end
 
   def self.all
-    @@all
-  end
-
-  def self.create(name)
-    artist = self.new(name)
-    artist.name = name
-    artist
-  end
-
-  def self.find(name)
-    self.all.detect { |artist| artist.name == name }
-  end
-
-  def self.find_or_create_by_name(name)
-    self.find(name) || self.create(name)
+    @@all    
   end
 
   def add_song(song)
-    @songs << song
+    song.artist = self
   end
 
-  def print_songs
-    songs.each { |song| puts song.name }
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find(name) ? self.find(name) : self.new(name)
+  end
+
+  def self.find(name)
+    self.all.find {|artist| artist.name == name }
   end
 
   def save
     @@all << self
   end
 
-  def songs
-    @songs
+  def print_songs
+    songs.each {|song| puts song.name}
   end
 end
